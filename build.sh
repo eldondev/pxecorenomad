ff43a5b5e0b26b38674f51a6e359a64d66127488
Eldon <eldon@eldondev.com>
2016-05-21 02:32:41 +0000

    Building a coreos oem for serf cluster manager

---
+++ build.sh
@@ -0,0 +1,7 @@
+set -x 
+set -e
+gunzip -fk coreos_production_pxe_image.cpio.gz  
+tar -czvf usr/share/oem/serf.tgz opt/ etc/ 
+find usr/  | cpio --verbose -o -A -H newc -O coreos_production_pxe_image.cpio 
+gzip -ck coreos_production_pxe_image.cpio >coreos.cpio.gz 
+qemu-system-x86_64 -machine accel=kvm -m 4G -smp 2 -kernel coreos_production_pxe.vmlinuz -initrd coreos.cpio.gz -append "coreos.autologin=tty1 console=tty1" -net nic -net tap,ifname=tap0,script=/bin/true,downscript=/bin/true
